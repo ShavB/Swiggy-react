d480ea33aadbd59c03a529644ae29a3df4d016b7
Shyam Bisht <bishtshyam135@gmail.com>
2023-11-10 21:42:21 +0000

    added search component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,45 @@
-import Slider from "../Slider/Slider";
 import Cards from "../Cards/Cards";
-import { restaurants }from "../../../Data/data";
-import './Home.css';
+import { restaurants } from "../../../Data/data";
+import "./Home.css";
+import { useState } from "react";
 
 const Home = () => {
-    restaurants.map((res) => {
-        console.log(res.info)
-    })
-    return(
-        <main className="main-body">
-            {/* <Slider /> */}
-            <div className="restaurant-list">
-            {restaurants.map((restaurant) => (
-                <Cards key={restaurant.info.id} {...restaurant.info} />
-            ))}
-            </div>
-        </main>
+  const [restaurantData, setRestaurantData] = useState(restaurants);
+  const [searchText, setSearchText] = useState("");
+
+  function searchBar(searchEle) {
+    setSearchText(searchEle);
+  }
+
+  const searchFilter = (searchData, restaurants) => {
+    const result = restaurants.filter((restaurant) =>
+      restaurant.info.name.toLowerCase().includes(searchData)
     );
-}
+    setRestaurantData(result);
+  };
+
+  return (
+    <main className="main-body">
+      {/* <Slider /> */}
+      <input
+        className="search-bar"
+        type="text"
+        value={searchText}
+        placeholder="search"
+        onChange={(e) => {
+          searchBar(e.target.value);
+        }}
+      />
+      <button onClick={() => searchFilter(searchText, restaurants)}>
+        Search
+      </button>
+      <div className="restaurant-list">
+        {restaurantData.map((restaurant) => (
+          <Cards key={restaurant.info.id} {...restaurant.info} />
+        ))}
+      </div>
+    </main>
+  );
+};
 
 export default Home;
